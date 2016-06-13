1f40757e9bbe2ff6520ba3c9e7f16cd1e015a58e
zaskar <zaskar_84@yahoo.com.ar>
2016-06-13 10:35:44 -0300

    fixed appending toolchain's bin dir to path

--- src/Toolchain.cpp
+++ src/Toolchain.cpp
@@ -156,7 +156,7 @@ void Toolchain::SetPaths() {
 			wxString new_path=original_path;
 			wxArrayString array; mxUT::Split(current_toolchain.bin_path,array,true,false);
 			for(unsigned int i=0;i<array.GetCount();i++) 
-				new_path=wxFileName(DIR_PLUS_FILE(config->zinjai_dir,array[i])).GetShortPath()+";"+new_path;
+				new_path=wxFileName(DIR_PLUS_FILE(config->zinjai_dir,array[i])).GetShortPath()+_if_win32(";",":")+new_path;
 			wxSetEnv("PATH",new_path);
 		} else {
 			wxSetEnv("PATH",original_path);
